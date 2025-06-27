c63dbe35c3c36c52141faecec6dc9015fb67c3ce
Chantalcou <chantiicou@gmail.com>
2025-06-27 12:17:31 -0300

    Add Asociado route

---
+++ api/routes/asociado.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Asociado = require('../models/Asociado');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    const nuevo = await Asociado.create(req.body);
+    res.status(201).json(nuevo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al crear asociado' });
+  }
+});
+
+module.exports = router;
