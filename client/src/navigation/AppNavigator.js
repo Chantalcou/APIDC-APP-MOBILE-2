5bb8a02d0042ea5d10787211ba3a82e746baf6d3
Chantalcou <chantiicou@gmail.com>
2025-06-30 13:00:39 -0300

    feat: show navbar globally

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -2,48 +2,17 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingScreen from '../screens/LandingScreen';
-import FormAsociationScreen from '../screens/FormAsociationScreen';
-
+import AssociateFormScreen from '../screens/AssociateFormScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Landing"
-        screenOptions={{
-          headerShown: false,
-          header: () => null,
-          title: '',
-          headerTitle: '',
-          headerTitleStyle: {
-            display: 'none',
-          },
-        }}
-      >
-        <Stack.Screen
-  name="FormAsociation"
-  component={FormAsociationScreen}
-  options={{
-    headerShown: false,
-  }}
-/>
-
-        <Stack.Screen
-          name="Landing"
-          component={LandingScreen}
-          options={{
-            headerShown: false,
-            header: () => null,
-            title: '',
-            headerTitle: '',
-            headerTitleStyle: {
-              display: 'none',
-            },
-          }}
-        />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="FormAsociation" component={AssociateFormScreen} />
+        <Stack.Screen name="Landing" component={LandingScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-} 
+}
