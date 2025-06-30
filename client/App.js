c2b283b4214d1165cdfb753f0ed30d43dbcf1624
Chantalcou <chantiicou@gmail.com>
2025-06-30 14:24:59 -0300

    fix: remove nested NavigationContainer warning

--- client/App.js
+++ client/App.js
@@ -1,33 +1,17 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { AuthProvider } from './src/context/AuthContext';
-import Navbar            from './src/components/Navbar';
-
-import LandingScreen      from './src/screens/LandingScreen';
-import AssociateFormScreen from './src/screens/AssociateFormScreen';
-// importa otras pantallas según las vayas creando
-// import SocioHomeScreen  from './src/screens/SocioHomeScreen';
-// import GestorDashboard  from './src/screens/GestorDashboardScreen';
-
-const Stack = createNativeStackNavigator();
+import Navbar from './src/components/Navbar';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          /* 👇  header global con tu Navbar  */
-          screenOptions={{
-            header: () => <Navbar />          // <- muestra Navbar en TODAS
-          }}
-        >
-          <Stack.Screen name="Landing"    component={LandingScreen} />
-          <Stack.Screen name="Formulario" component={AssociateFormScreen} />
-
-        </Stack.Navigator>
+        <Navbar />
+        <AppNavigator />
       </NavigationContainer>
     </AuthProvider>
   );
